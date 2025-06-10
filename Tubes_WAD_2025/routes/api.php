30a9b286d51d3e895a1a8ed802d80fcf9f97ebe9
Jay Davinci <jaydavinciii11@gmail.com>
2025-06-10 21:04:20 +0700

    commit api

--- Tubes_WAD_2025/routes/api.php
+++ Tubes_WAD_2025/routes/api.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AcaraController;
 use App\Http\Controllers\KeaktifanController;
 use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\TerlambatController;
+
 
 Route::get('/acara', [AcaraController::class, 'getListAcara']);
 Route::get('/keaktifan', [KeaktifanController::class, 'getListKeaktifan']);
 Route::get('/profil', [ProfilController::class, 'getListProfil']);
+Route::get('/terlambat', [TerlambatController::class], 'getListTerlambat');
