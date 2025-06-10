e8c43da34c6d61f9b1aad182511eac8ed323fc0b
setyoriniokvi <setyoriniokviana@gmail.com>
2025-06-10 14:08:27 +0700

    api/profil

--- Tubes_WAD_2025/routes/api.php
+++ Tubes_WAD_2025/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AcaraController;
 use App\Http\Controllers\KeaktifanController;
+use App\Http\Controllers\ProfilController;
 
 Route::get('/acara', [AcaraController::class, 'getListAcara']);
 Route::get('/keaktifan', [KeaktifanController::class, 'getListKeaktifan']);
+Route::get('/profil', [ProfilController::class, 'getListProfil']);
