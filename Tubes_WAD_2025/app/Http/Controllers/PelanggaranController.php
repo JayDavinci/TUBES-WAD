72401f6809bf19e738b18c89d2677b6c24d27f80
Ahnaffauzi <158241489+Ahnaffauzi@users.noreply.github.com>
2025-06-12 04:31:19 +0700

    fix api

--- Tubes_WAD_2025/app/Http/Controllers/PelanggaranController.php
+++ Tubes_WAD_2025/app/Http/Controllers/PelanggaranController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pelanggaran;
 use App\Models\AnggotaAsrama;
+use App\Http\Resources\PelanggaranResource;
 
 class PelanggaranController extends Controller
 {
